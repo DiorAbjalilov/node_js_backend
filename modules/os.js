fdc6a36901715a558e01f835d3df50290001d257
DiorAbjalilov <abjalilovdiyor@gmail.com>
2022-01-14 23:09:00 +0500

    add os file

---
+++ modules/os.js
@@ -0,0 +1,16 @@
+const os = require("os");
+
+// Operatsion Systema
+// console.log(os.platform());
+
+// Arch x64 or x32 raziriyad
+// console.log(os.arch());
+
+// CPU about
+// console.log(os.cpus());
+
+// Free mem
+// console.log(os.freemem());
+
+// Total mem
+// console.log(os.totalmem());
