2e51ff70337e6de6d94f6848837fad3dd5fd9023
DiorAbjalilov <abjalilovdiyor@gmail.com>
2022-01-16 09:40:23 +0500

    add new node js rest api

--- modules/http.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  res.write("server ishladi");
-  res.end();
-});
-
-server.listen(1111, () => console.log("node server ishladi"));
